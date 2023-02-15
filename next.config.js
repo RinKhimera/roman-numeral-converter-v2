7cf2f4e755ef187f12ae33f5619ef82b2be3413b
Samuel Pokam <dixiades@gmail.com>
2023-02-15 15:09:31 +0100

    updates

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
 };
 
-// eslint-disable-next-line no-undef
 module.exports = nextConfig;
